735e5b9d2e0b89a035dc53036804f214d442c78f
saied <saeedsayed077@gmail.com>
2024-02-13 22:29:45 +0200

    update with Visit model and visits Table

---
+++ database/migrations/2024_02_06_151647_create_visits_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisitsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('place_id');
+            $table->foreign('place_id')->references('id')->on('Place');
+            $table->dateTime('visited_at');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('visits');
+    }
+};
