daee0eb87f99878485b736c9855567aa186973a0
Rahma-Abo-Helal <abohrahmaahmed@gmail.com>
2024-02-23 20:02:05 +0200

    ADD

---
+++ app/Models/Employee.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+
+{
+
+    use HasFactory;
+    public $timestamps = false;
+    protected $fillable = ['name', 'service_id', 'department', 'photo'];
+    ////belong
+    // public function location(){
+    //     return $this->belongsTo(Location::class);
+    // }
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+    // public function building(){
+    //     return $this->belongsTo(Building::class);
+    // }
+}
---
+++ app/Models/Service.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Service extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $fillable = ['name', 'location_id', 'description'];
+
+    /////has
+    public function employee()
+    {
+        return $this->hasMany(Employee::class);
+    }
+
+    ////belong
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+}
