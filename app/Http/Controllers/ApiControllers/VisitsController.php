406cc3d63aa77b51421763e709a1369aec3cf972
saeed elsayed ibrahim goma <128707838+saie3d@users.noreply.github.com>
2024-07-02 03:47:05 +0300

    Update VisitsController.php

--- app/Http/Controllers/ApiControllers/VisitsController.php
+++ app/Http/Controllers/ApiControllers/VisitsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\ApiControllers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreVisitRequest;
 use App\Models\Visit;
-use App\Models\Place; // Assuming you have a Place model
+use App\Models\Place;
 use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
